96a0fe6b4eec151c98b8f3b96cd02af0ef15974b
Umme Fatima Sadia Hossain <133772958+Jxxy123@users.noreply.github.com>
2026-07-17 23:57:40 +0600

    Create robots.ts

---
+++ app/robots.ts
@@ -0,0 +1,12 @@
+import type { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+    },
+    sitemap:
+      "https://umme-fatima-portfolio.vercel.app/sitemap.xml",
+  };
+}
